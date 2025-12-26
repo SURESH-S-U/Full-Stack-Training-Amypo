f8b44fbf266d83c86016c84665d5fc491cae019c
SURESH-S-U <suresh.elite07@gmail.com>
2025-12-26 11:39:33 +0530

    Pages Added

---
+++ MERN Project/react_route/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <h1>Welcome to ABout Page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque ea unde eaque explicabo doloribus doloremque, dolorem totam sint quas magnam!</p>
+    </div>
+  )
+}
+
+export default About
--- MERN Project/react_route/src/pages/Contact.jsx
+++ MERN Project/react_route/src/pages/Contact.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const Contact = () => {
+function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+      <h1>Welcome to Contact Page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt cumque laboriosam fugiat quas ratione aliquid eius esse quo sit error?</p>
+    </div>
   )
 }
 
