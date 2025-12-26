dd604be32f087f0671d2e875730fafa078c60403
SURESH-S-U <suresh.elite07@gmail.com>
2025-12-26 09:58:16 +0530

    Header and Footer Created

---
+++ MERN Project/react_route/src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+      <h1>Bannari Amman Institute Of Technology</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium blanditiis nam enim aut quaerat numquam! Error expedita neque nesciunt quaerat.</p>
+    </div>
+  )
+}
+
+export default Main
