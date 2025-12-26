97ad32c2f0377f90c8a1e449999b4c5698dda825
SURESH-S-U <suresh.elite07@gmail.com>
2025-12-26 15:39:13 +0530

    Routers Added

--- MERN Project/react_bootstrap/src/App.jsx
+++ MERN Project/react_bootstrap/src/App.jsx
@@ -1,14 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter , Routes , Route , Link} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element=/>
-      </Routes>
-    </BrowserRouter>
+    <div>
+
+      {/* It is Used to Navigation */}
+      <BrowserRouter>
+
+        {/* Creating Links For Created URL Paths */}
+        <nav>
+          
+          <Link to='/'>Home</Link>
+          <Link to='/About'>About</Link>
+          <Link to='/Contact'>Contact</Link>
+
+        </nav>
+
+        {/* Creating URL Paths for Each page */}
+        <Routes>
+
+          <Route path='/' element={<Home/>} />
+          <Route path='/About' element={<About/>} />
+          <Route path='/Contact' element={<Contact/>} />
+
+        </Routes>
+
+      </BrowserRouter>
+    </div>
   );
 }
 
