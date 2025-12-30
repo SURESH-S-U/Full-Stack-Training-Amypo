ab54d3aca390cfc6725b4a640720771580f7e908
SURESH-S-U <suresh.elite07@gmail.com>
2025-12-30 10:58:05 +0530

    Authentication context was Created

---
+++ MERN Project/context/src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const userName = "Suresh";
+
+
+  return (
+    <div className="App">
+      
+      
+
+    </div>
+  );
+}
+
+export default App;
