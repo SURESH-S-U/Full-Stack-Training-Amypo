3c6ad2be57139070a3c64a266a4f5a410dfeb0b1
SURESH-S-U <suresh.elite07@gmail.com>
2026-01-05 13:47:29 +0530

    Material UI starts

---
+++ MERN Project/advanced_routing/src/context/AuthContext.jsx
@@ -0,0 +1,36 @@
+import { createContext, useState } from "react";
+
+// Create a constext onject for Authentication
+// This will store and share the login data globally
+
+export const AuthContext = createContext();
+
+
+
+const AuthProvider = ({children}) => {
+
+    // Create a state to store the logged in information.
+    const [user , setUser] = useState(null);
+
+    // Function to login the user
+    const login = (name) => {
+        setUser(name);
+    }
+
+    // Function to logOut the user
+    const logout = () => {
+        setUser(null);
+    }
+
+  
+  
+  
+    return (
+        
+        <AuthContext.Provider value={{user, login, logout}}>
+            {children}      {/* All wrapped components can aceess */}
+        </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider
