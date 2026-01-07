3bc5a5e11d9c1d7e0c9bfd87a681622c113ea2cf
SURESH-S-U <suresh.elite07@gmail.com>
2026-01-07 14:25:21 +0530

    axios Updated

---
+++ MERN Project/axios/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Posts from './components/Posts';
+import Comments from './components/Comments';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Posts/>
+      <Comments/>
+
+    </div>
+  );
+}
+
+export default App;
