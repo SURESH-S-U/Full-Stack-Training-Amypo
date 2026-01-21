dfb84b211bf1044053ad7ed5e9189aeb6c37c6d1
SURESH-S-U <suresh.elite07@gmail.com>
2026-01-21 11:06:10 +0530

    Frontend completed and Backend Initialized

--- MERN Project/jwt/client/src/App.jsx
+++ MERN Project/jwt/client/src/App.jsx
@@ -1,11 +1,12 @@
-const { useState } = require("react");
+import axios from "axios";
+import { useState } from "react";
 
 // Backend Server API base URL
 const API = "http://localhost:4000";
 
 
 // Main React Component
-function app(){
+function App(){
 
   // Create a state to store the login status - Login Mode or Register mode.
   const [isLogin , setIsLogin] = useState(true);
@@ -15,6 +16,14 @@ function app(){
 
   // State to store the JWT Token(Read from browser local storage).
   const [token , setToken] = useState(localStorage.getItem("token"));
+
+
+  // Register Function
+  const register = async() => {
+    // Send Form Data through Axios request
+    await axios.post(`${API}/register`,form);
+  }
+
 
   // Logged UI - After Login
   if(token){    
@@ -28,9 +37,34 @@ function app(){
 
   return (
     <div>
-       <h2>{isLogin ? "Login" : "Register"}</h2>
-       {isLogin}
+
+      {/* Login Page - Email and Password is enough
+          Register Page - Name , Email and Password needed */}
+
+      <h2>{isLogin ? "Login" : "Register"}</h2>
+
+      {!isLogin && ( // It is also a type of conditional rendering.
+                     // It is Best When you have Only if not If else.
+          <input type="text" placeholder="Name" onChange={(e) => setForm({...form , name: e.target.value})} />
+      )}
+       
+      <input type="email" placeholder="Enter Email" onChange={(e) => setForm({...form, email: e.target.value})} />
+
+      <input type="password" placeholder="Password" onChange={(e) => setForm({...form, password: e.target.value})} />
+
+      {isLogin ?
+        (<button>Login</button>)
+        :
+        (<button>Register</button>)
+      }
+
+      <p onClick={() => setIsLogin(!isLogin)} style={{cursor:"pointer"}}>
+        {isLogin ? "Create Account" : "Already have an account ? Login"}
+      </p>
+
     </div>
   );
 
 }
+
+export default App;
